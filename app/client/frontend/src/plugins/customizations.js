3d3548057c836a464d596370b54069c92328caeb
Omri Har-Shemesh <omrihar@gmail.com>
2018-09-01 22:09:03 +0200

    Add support to markdown, prism.js and katex

---
+++ app/client/frontend/src/plugins/customizations.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// import something here
+require('assets/js/prism.js')
+require('assets/css/prism.css')
+//require('assets/css/katex.min.css')
+require('../../node_modules/katex/dist/katex.min.css')
+
+// leave the export, even if you don't use it
+export default ({ app, router, Vue }) => {
+  // something to do
+}
